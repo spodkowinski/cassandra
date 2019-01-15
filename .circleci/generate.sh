f15f738de29bfa56eabab2b251d489a3f66d3a89
Marcus Eriksson <marcuse@apache.org>
2019-01-15 11:31:52 +0100

    upgradetests

--- .circleci/generate.sh
+++ .circleci/generate.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-if [ -s config-2_1.yml ]; then
-    circleci config process config-2_1.yml > config.yml.LOWRES
-    patch -o config-2_1.yml.HIGHRES config-2_1.yml config-2_1.yml.high_res.patch
-    circleci config process config-2_1.yml.HIGHRES > config.yml.HIGHRES
-    rm config-2_1.yml.HIGHRES
-else
-    echo "Run this in cassandra/.circleci directory"
-    exit -1
-fi
+BASEDIR=`dirname $0`
+
+circleci config process $BASEDIR/config-2_1.yml > $BASEDIR/config.yml.LOWRES
+patch -o $BASEDIR/config-2_1.yml.HIGHRES $BASEDIR/config-2_1.yml $BASEDIR/config-2_1.yml.high_res.patch
+circleci config process $BASEDIR/config-2_1.yml.HIGHRES > $BASEDIR/config.yml.HIGHRES
+rm $BASEDIR/config-2_1.yml.HIGHRES
+
